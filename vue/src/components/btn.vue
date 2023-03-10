a493b0a8853d62ffc1e4c1ae5dfd7db0e064ff69
wersoam <cowistrash@gmail.com>
2023-03-10 10:15:00 -0500

    more testing

---
+++ vue/src/components/btn.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button type="button" class="btn" >
+      {{ number }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "button",
+  prop: {
+    number: String,
+  },
+};
+</script>
+<style scoped>
+.btn {
+  background-color: rgb(227, 196, 255);
+  font-size: 35px;
+}
+</style>
