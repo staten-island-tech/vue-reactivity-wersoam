9536cf4db54c83d025ad3f47599f50976b5eea45
wersoam <cowistrash@gmail.com>
2023-03-21 10:13:19 -0400

    save

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,103 +1,17 @@
 <script setup>
-import answer from "./components/answerdis.vue";
-import btn from "./components/btncli.vue";
-
-const box = { box: document.getElementById("box") };
-const clear = function () {
-  document.getElementById("box").innerHTML = "";
-};
-const click1 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `1`);
-};
-const click2 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `2`);
-};
-const click3 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `3`);
-};
-const click4 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `4`);
-};
-const click5 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `5`);
-};
-const click6 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `6`);
-};
-const click7 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `7`);
-};
-const click8 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `8`);
-};
-const click9 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `9`);
-};
-const click0 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `0`);
-};
-const click00 = function () {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `00`);
-};
-const clickpoint = function () {
-  if (this.document.getElementById("box").includes(".")) return;
-  document.getElementById("box").insertAdjacentHTML("beforeend", `.`);
-};
-const clickadd = function (a, b) {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `+`);
-};
-const clickminus = function (a, b) {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `-`);
-};
-const clicktimes = function (a, b) {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `*`);
-};
-const clickdiv = function (a, b) {
-  document.getElementById("box").insertAdjacentHTML("beforeend", `/`);
-};
-
-const enter = function () {
-  let equation = document.getElementById("box").stringify;
-  let answer = eval(equation);
-  document.getElementById("box").innerHTML = answer;
+import answer from "./components/calculator.vue";
+</script>
+<script>
+export default {
+  name: "app",
+  components: {
+    calculator,
+  },
 };
 </script>
 
 <template>
-  <div>
-    <div id="box">{{ number || 0 }}</div>
-    <div class="press">
-      <div>
-        <h1>simple calculator</h1>
-        <btn @click="click1()" symbols="1"></btn>
-        <btn @click="click2()" symbols="2"></btn>
-        <btn @click="click3()" symbols="3"></btn>
-        <btn @click="clickadd()" symbols="+"></btn>
-      </div>
-      <div>
-        <btn @click="click4()" symbols="4"></btn>
-        <btn @click="click5()" symbols="5"></btn>
-        <btn @click="click6()" symbols="6"></btn>
-        <btn @click="clickminus()" symbols="-"></btn>
-      </div>
-      <div>
-        <btn @click="click7()" symbols="7"></btn>
-        <btn @click="click8()" symbols="8"></btn>
-        <btn @click="click9()" symbols="9"></btn>
-        <btn @click="clicktimes()" symbols="*"></btn>
-      </div>
-      <div>
-        <btn @click="click00()" symbols="00"></btn>
-        <btn @click="click0()" symbols="0"></btn>
-        <btn @click="clickpoint()" symbols="."></btn>
-        <btn @click="clickdiv()" symbols="/"></btn>
-      </div>
-      <div>
-        <btn @click="clear()" symbols="clear"></btn>
-        <btn @click="enter()" symbols="enter"></btn>
-      </div>
-    </div>
-  </div>
+  <HelloWorld />
 </template>
 
 <style scoped>
---
+++ vue/src/components/calculator.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div id="box">{{ number || 0 }}</div>
+    <div class="press">
+      <div>
+        <h1>simple calculator</h1>
+        <button @click="click1()" symbols="1"></button>
+        <button @click="click2()" symbols="2"></button>
+        <button @click="click3()" symbols="3"></button>
+        <button @click="clickadd()" symbols="+"></button>
+      </div>
+      <div>
+        <button @click="click4()" symbols="4"></button>
+        <button @click="click5()" symbols="5"></button>
+        <button @click="click6()" symbols="6"></button>
+        <button @click="clickminus()" symbols="-"></button>
+      </div>
+      <div>
+        <button @click="click7()" symbols="7"></button>
+        <button @click="click8()" symbols="8"></button>
+        <button @click="click9()" symbols="9"></button>
+        <button @click="clicktimes()" symbols="*"></button>
+      </div>
+      <div>
+        <button @click="click00()" symbols="00"></button>
+        <button @click="click0()" symbols="0"></button>
+        <button @click="clickpoint()" symbols="."></button>
+        <button @click="clickdiv()" symbols="/"></button>
+      </div>
+      <div>
+        <button @click="clear()" symbols="clear"></button>
+        <button @click="enter()" value="enter"></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "answer",
+  prop: {
+    number: String,
+  },
+  methods: {
+    clear() {
+      this.number = "";
+    },
+    add() {
+      this.number;
+    },
+    minus() {
+      this.number;
+    },
+    divide() {
+      this.number;
+    },
+    mutiply() {
+      this.number;
+    },
+    one() {
+      this.number;
+    },
+  },
+};
+</script>
