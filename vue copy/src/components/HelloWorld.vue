1c69f8a3777462c9c4dea58b03413d895aaddb73
wersoam <cowistrash@gmail.com>
2023-03-22 10:15:56 -0400

    the end is near

--- vue copy/src/components/HelloWorld.vue
+++ vue copy/src/components/HelloWorld.vue
@@ -1,31 +1,35 @@
 <template>
   <div>
-    <div id="box">{{ answer || 0 }}</div>
+    <div>{{ calcsign }}</div>
+    <div id="box">
+      <div>{{ calc1 }}</div>
+      <div>{{ answer || 0 }}</div>
+    </div>
     <div class="press">
       <div>
         <h1>simple calculator</h1>
         <button @click="numberup('1')">1</button>
         <button @click="numberup('2')">2</button>
         <button @click="numberup('3')">3</button>
-        <button @click="clickadd()">+</button>
+        <button @click="add()">+</button>
       </div>
       <div>
         <button @click="numberup('4')">4</button>
         <button @click="numberup('5')">5</button>
         <button @click="numberup('6')">6</button>
-        <button @click="clickminus()">-</button>
+        <button @click="minus()">-</button>
       </div>
       <div>
         <button @click="numberup('7')">7</button>
         <button @click="numberup('8')">8</button>
         <button @click="numberup('9')">9</button>
-        <button @click="clicktimes()">*</button>
+        <button @click="mutiply()">*</button>
       </div>
       <div>
         <button @click="numberup('00')">00</button>
         <button @click="numberup('0')">0</button>
         <button @click="numberdot()">.</button>
-        <button @click="clickdiv()">/</button>
+        <button @click="divide()">/</button>
       </div>
       <div>
         <button @click="clear()">clear</button>
@@ -39,28 +43,45 @@
 export default {
   name: "answer",
   data() {
-    return { answer: "" };
+    return {
+      answer: "",
+      calc1: "",
+      calcsign: "",
+    };
   },
   methods: {
     clear() {
       this.answer = "";
+      (this.calcsign = ""), (this.calc1 = "");
+    },
+    enter() {
+      if (this.calcsign.indexOf("+") === 0) {
+        this.answer = +this.calc1 + +this.answer;
+        this.calc1 = "";
+      } else if (this.calcsign.indexOf("-") === 0) {
+        this.answer = this.calc1 - this.answer;
+        this.calc1 = "";
+      }
     },
     add() {
-      this.answer;
+      this.calc1 = this.answer;
+      this.calcsign = "+";
     },
     minus() {
-      this.answer;
+      this.calc1 = this.answer;
+      this.calcsign = "-";
     },
     divide() {
-      this.answer;
+      this.calc1 = this.answer;
+      this.calcsign = "/";
     },
     mutiply() {
-      this.answer;
+      this.calc1 = this.answer;
+      this.calcsign = "*";
     },
     numberdot() {
-      if (this.number.indexOf(".") === -1) {
+      if (this.answer.indexOf(".") === -1) {
         this.answer = this.answer + ".";
-      } else {
       }
     },
 
